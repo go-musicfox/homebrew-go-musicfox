7672959f50a5c70bbbbdc4b08bb34942c97b8372
anhoder <alan1766447919@gmail.com>
2021-07-18 23:57:30 +0800

    Update go-musicfox.rb

--- go-musicfox.rb
+++ go-musicfox.rb
@@ -4,10 +4,12 @@
 class GoMusicfox < Formula
   desc "go-musicfox是musicfox的重写版，使用Go编写的网易云音乐命令行程序。"
   homepage "https://github.com/anhoder/go-musicfox"
-  url "https://github.com/anhoder/go-musicfox/releases/download/v2.0.1/musicfox.mac"
-  version "v2.0.1"
-  sha256 "b3a731d16fdd5619d4cf2bb096946c3513259dacc377eea26590c1e331074a0c"
+  url "https://github.com/anhoder/go-musicfox/releases/download/v2.1.0/musicfox.mac"
+  version "v2.1.0"
+  sha256 "703c102a30b5c9cd3525f6b9830ebb77d82007bca7bc8e8ae63179f18e430c4e"
 
+  depends_on "terminal-notifier"
+  
   def install
     system 'mv musicfox.mac musicfox'
     system 'echo', '-e', "\033[1;33m给个star✨吧~\033[0m \033[4;36mhttps://github.com/anhoder/go-musicfox \033[0m"
