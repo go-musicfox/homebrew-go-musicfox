b32cd9b423f30aa14cc1345a76b2f5f7ca2e1819
anhoder <anhoder@88.com>
2022-10-03 12:43:33 +0800

    Brew formula update for go-musicfox version v3.1.0

--- go-musicfox.rb
+++ go-musicfox.rb
@@ -5,21 +5,21 @@
 class GoMusicfox < Formula
   desc "go-musicfox是musicfox的重写版，使用Go编写的网易云音乐命令行程序。"
   homepage "https://github.com/anhoder/go-musicfox"
-  version "3.0.2"
+  version "3.1.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/anhoder/go-musicfox/releases/download/v3.0.2/go-musicfox_3.0.2_darwin_amd64.zip"
-      sha256 "1e2db45afcb67a7424f33c973589ee27b621b418754a222a1edf71928b08ff1f"
+    if Hardware::CPU.arm?
+      url "https://github.com/anhoder/go-musicfox/releases/download/v3.1.0/go-musicfox_3.1.0_darwin_arm64.zip"
+      sha256 "4b8462308cb3f3f10eb0fd7970b93b9a1be3bcb4865530b16627adce3d20e835"
 
       def install
         system 'echo', '-e', "\033[1;33m给个star✨吧~\033[0m \033[4;36mhttps://github.com/anhoder/go-musicfox \033[0m"
         bin.install 'musicfox'
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/anhoder/go-musicfox/releases/download/v3.0.2/go-musicfox_3.0.2_darwin_arm64.zip"
-      sha256 "9d77826dc9a87e6d3cfffd906c5b40556767c0c295ad8be7d900bdf901903405"
+    if Hardware::CPU.intel?
+      url "https://github.com/anhoder/go-musicfox/releases/download/v3.1.0/go-musicfox_3.1.0_darwin_amd64.zip"
+      sha256 "5ed508787e474a6bec9971d09d3650f9649d035f4a19465b534fd13068a1dcb0"
 
       def install
         system 'echo', '-e', "\033[1;33m给个star✨吧~\033[0m \033[4;36mhttps://github.com/anhoder/go-musicfox \033[0m"
@@ -30,8 +30,8 @@ class GoMusicfox < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/anhoder/go-musicfox/releases/download/v3.0.2/go-musicfox_3.0.2_linux_amd64.zip"
-      sha256 "b7c262aedd92813dc1e0b46c7f9d2b15a466e6a27f6190aa2a82a003c62eec4d"
+      url "https://github.com/anhoder/go-musicfox/releases/download/v3.1.0/go-musicfox_3.1.0_linux_amd64.zip"
+      sha256 "49a8c5ab055feb65aecfe7068f4ef9da11884c234b4c633018adac13771ec5d8"
 
       def install
         system 'echo', '-e', "\033[1;33m给个star✨吧~\033[0m \033[4;36mhttps://github.com/anhoder/go-musicfox \033[0m"
