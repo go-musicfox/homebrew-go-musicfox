e0fb2e2b010089ba0eb0db9ac50034d8013c0e1b
anhoder <anhoder@88.com>
2026-04-18 14:05:35 +0800

    Brew cask update for go-musicfox version v4.8.5

--- Casks/go-musicfox-macapp.rb
+++ Casks/go-musicfox-macapp.rb
@@ -2,41 +2,28 @@
 cask "go-musicfox-macapp" do
   desc "go-musicfox MacApp版本，使用Go编写的网易云音乐命令行程序。"
   homepage "https://github.com/go-musicfox/go-musicfox"
-  version "4.8.4"
+  version "4.8.5"
 
   livecheck do
     skip "Auto-generated on release."
   end
 
-  binary "go-musicfox-macapp"
+  binary "musicfox.app/Contents/MacOS/musicfox"
 
   on_macos do
     on_intel do
-      url "https://github.com/go-musicfox/go-musicfox/releases/download/v4.8.4/go-musicfox_4.8.4_darwin_amd64.app.zip"
-      sha256 "fb94eb8bf47b6bb523221582a8d4d40f945e2b74584df6691b631f1523ce669e"
+      url "https://github.com/go-musicfox/go-musicfox/releases/download/v4.8.5/go-musicfox_4.8.5_darwin_amd64_macapp.zip"
+      sha256 "d454514e88dfdd0d8116a136dee03dbde8177db5a0411cdf36581ef605348e25"
     end
     on_arm do
-      url "https://github.com/go-musicfox/go-musicfox/releases/download/v4.8.4/go-musicfox_4.8.4_darwin_arm64.app.zip"
-      sha256 "0158b51104a7c84b8ad5685c54fae4772a4d8afcac28511125f004d97deaf136"
+      url "https://github.com/go-musicfox/go-musicfox/releases/download/v4.8.5/go-musicfox_4.8.5_darwin_arm64_macapp.zip"
+      sha256 "bab868d80d33fa3e87a93eecc404d3cdd8a12d02b6687a213a98f9e551bfe790"
     end
   end
 
   postflight do
     system 'xattr', '-r', '-d', 'com.apple.quarantine', '--', "#{caskroom_path}/#{version}/musicfox.app"
-
-    # 定义源二进制文件的路径
-    source = "#{caskroom_path}/#{version}/musicfox.app/Contents/MacOS/musicfox-app.wrapper.sh"
-    # 定义目标命令的路径
-    target = ENV["HOMEBREW_PREFIX"] + "/bin/musicfox"
-
-    # 创建符号链接
-    FileUtils.ln_sf(source, target)
-
     system 'echo', '-e', "\033[1;33m给个star✨吧~\033[0m \033[4;36mhttps://github.com/go-musicfox/go-musicfox \033[0m"
-  end
-
-  uninstall_postflight do
-    FileUtils.rm_f(ENV["HOMEBREW_PREFIX"] + "/bin/musicfox")
   end
 
   # No zap stanza required
